336263dfc43e332654914b824d72a01a8e24e9f2
Álvaro <lvaro@DESKTOP-HU9E7D0>
2025-06-23 13:03:33 +0200

    Arreglamos conexion a BBDD Jul.

--- ApacheLogDataMiningJul-acueva/src/main/java/com/gsd/daw/data/DatabaseConnector.java
+++ ApacheLogDataMiningJul-acueva/src/main/java/com/gsd/daw/data/DatabaseConnector.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 public class DatabaseConnector {
     public static Connection getConnection(String ip, String instance, String user, String password) throws SQLException {
     	String url = "jdbc:oracle:thin:@//" + ip + ":1521/" + instance;
-        return DriverManager.getConnection(url, user, password);
+        return DriverManager.getConnection(url, user, password); 
     }
 }
